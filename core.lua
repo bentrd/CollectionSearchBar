7bc384753a37055504d5f2d466c773bb57313f67
bentrd <benjamin@tordjman.com>
2024-06-07 20:12:37 +0200

    removed useless comments

--- core.lua
+++ core.lua
@@ -1,28 +1,5 @@
 G.ARGS = {collection_search_input = ''}
 
--- local create_UIBox_your_collection_ref = create_UIBox_your_collection
--- function create_UIBox_your_collection()
---     local t = create_UIBox_your_collection_ref()
---     local contents = t.nodes[1].nodes[1].nodes[1]
---     local back_btn = t.nodes[1].nodes[1].nodes[2]
-
---     t.nodes[1].nodes[1].nodes[1] = {n = G.UIT.R, config = { align = "cm", minw = 2.5, padding = 0, r = 0.1 }, nodes = {
---         {n = G.UIT.C, config = { align = "cm", padding = 0 }, nodes = {
---             create_text_input({ all_caps = true, ref_table = G.ARGS, collection_search_ref_value = 'input', extended_corpus = true }),
---         }},
---         {n = G.UIT.C, config = { id = 'your_collection_search', align = "cm", maxw = 1, padding = 0.1, r = 0.1, hover = true, colour = G.C.RED, button = 'your_collection_search', shadow = true }, nodes = {
---             {n = G.UIT.R, config = { align = "cm", padding = 0, no_fill = true }, nodes = {
---                 {n = G.UIT.T, config = { id = 'your_collection_search', text = 'Search', scale = 0.5, colour = G.C.UI.TEXT_LIGHT, shadow = true } }
---             }}
---         }}
---     }}
-
---     t.nodes[1].nodes[1].nodes[2] = contents
---     t.nodes[1].nodes[1].nodes[3] = back_btn
-
---     return t
--- end
-
 function create_UIBox_your_collection_search()
     local deck_tables = {}
 
@@ -154,34 +131,3 @@ G.FUNCS.your_collection_search = function(e)
         definition = create_UIBox_your_collection_search(),
     }
 end
-
---[[
- {n=G.UIT.R, config={align = "m", padding = 0.05}, nodes = {
-            create_text_input({prompt_text = 'Joker key', ref_table = G, ref_value = 'ARGS_icollection_search_nput', text_scale = 0.3, w = 5, h = 0.6})
-        }},
-        {n=G.UIT.C, config={align = "cm", padding = 0.15}, nodes={
-            UIBox_button({button = 'your_collection_jokers', label = {localize('b_jokers')}, count = G.DISCOVER_TALLIES.jokers,  minw = 5, minh = 1.7, scale = 0.6, id = 'your_collection_jokers'}),
-            UIBox_button({button = 'your_collection_decks', label = {localize('b_decks')}, count = G.DISCOVER_TALLIES.backs, minw = 5}),
-            UIBox_button({button = 'your_collection_vouchers', label = {localize('b_vouchers')}, count = G.DISCOVER_TALLIES.vouchers, minw = 5, id = 'your_collection_vouchers'}),
-            {n=G.UIT.R, config={align = "cm", padding = 0.1, r=0.2, colour = G.C.BLACK}, nodes={
-                {n=G.UIT.C, config={align = "cm", maxh=2.9}, nodes={
-                {n=G.UIT.T, config={text = localize('k_cap_consumables'), scale = 0.45, colour = G.C.L_BLACK, vert = true, maxh=2.2}},
-                }},
-                {n=G.UIT.C, config={align = "cm", padding = 0.15}, nodes={
-                UIBox_button({button = 'your_collection_tarots', label = {localize('b_tarot_cards')}, count = G.DISCOVER_TALLIES.tarots, minw = 4, id = 'your_collection_tarots', colour = G.C.SECONDARY_SET.Tarot}),
-                UIBox_button({button = 'your_collection_planets', label = {localize('b_planet_cards')}, count = G.DISCOVER_TALLIES.planets, minw = 4, id = 'your_collection_planets', colour = G.C.SECONDARY_SET.Planet}),
-                UIBox_button({button = 'your_collection_spectrals', label = {localize('b_spectral_cards')}, count = G.DISCOVER_TALLIES.spectrals, minw = 4, id = 'your_collection_spectrals', colour = G.C.SECONDARY_SET.Spectral}),
-                }}
-            }},
-        }},
-        {n=G.UIT.C, config={align = "cm", padding = 0.15}, nodes={
-            UIBox_button({button = 'your_collection_enhancements', label = {localize('b_enhanced_cards')}, minw = 5}),
-            UIBox_button({button = 'your_collection_seals', label = {localize('b_seals')}, minw = 5, id = 'your_collection_seals'}),
-            UIBox_button({button = 'your_collection_editions', label = {localize('b_editions')}, count = G.DISCOVER_TALLIES.editions, minw = 5, id = 'your_collection_editions'}),
-            UIBox_button({button = 'your_collection_boosters', label = {localize('b_booster_packs')}, count = G.DISCOVER_TALLIES.boosters, minw = 5, id = 'your_collection_boosters'}),
-            UIBox_button({button = 'your_collection_tags', label = {localize('b_tags')}, count = G.DISCOVER_TALLIES.tags, minw = 5, id = 'your_collection_tags'}),
-            UIBox_button({button = 'your_collection_blinds', label = {localize('b_blinds')}, count = G.DISCOVER_TALLIES.blinds, minw = 5, minh = 2.0, id = 'your_collection_blinds', focus_args = {snap_to = true}}),
-        }},
-
-    }})
-]]
