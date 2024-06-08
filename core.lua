d214c6458aa1ff5735267a3458f265dad16f90c4
bentrd <benjamin@tordjman.com>
2024-06-08 20:54:38 +0200

    cleaned up

--- core.lua
+++ core.lua
@@ -1,5 +1,32 @@
 G.ARGS = {collection_search_input = ''}
 
+G.FUNCS.search_bar = function ()
+    local search_bar = {n = G.UIT.R, config = { align = 'cm', padding = 0.2 }, nodes = {
+        create_text_input({ prompt_text = 'Search for jokers or consumables', max_length = 32, ref_table = G.ARGS, ref_value = 'collection_search_input', extended_corpus = true, w = 6 }),
+        {n = G.UIT.C, config = { id = 'your_collection_search', align = 'cm', maxw = 1, r = 0.1, hover = true, colour = G.C.RED, button = 'your_collection_search', shadow = true }, nodes = {
+            {n = G.UIT.R, config = { align = 'cm', padding = 0.1, no_fill = true }, nodes = {
+                {n = G.UIT.T, config = { id = 'your_collection_search', text = 'Search', scale = 0.5, colour = G.C.UI.TEXT_LIGHT, shadow = true } }
+            }}
+        }}
+    }}
+    return search_bar
+end
+
+function get_matched_items()
+    local matched_items = {}
+    local search_input = string.lower(G.ARGS['collection_search_input'])
+    local pools = {"Joker", "Tarot", "Spectral", "Planet"}
+
+    for _, pool in ipairs(pools) do
+        for _, v in ipairs(G.P_CENTER_POOLS[pool]) do
+            if string.find(string.lower(v.name), search_input) then
+                table.insert(matched_items, v)
+            end
+        end
+    end
+    return matched_items
+end
+
 function create_UIBox_your_collection_search()
     local deck_tables = {}
 
@@ -11,37 +38,13 @@ function create_UIBox_your_collection_search()
             0.95 * G.CARD_H,
             { card_limit = 5, type = 'title', highlight_limit = 0, collection = true })
         table.insert(deck_tables,
-            {
-                n = G.UIT.R,
-                config = { align = "cm", padding = 0.07, no_fill = true },
-                nodes = {
-                    { n = G.UIT.O, config = { object = G.your_collection[j] } }
-                }
-            }
+            {n = G.UIT.R,config = { align = "cm", padding = 0.07, no_fill = true },nodes = {
+                {n = G.UIT.O, config = { object = G.your_collection[j] }}
+            }}
         )
     end
 
-    local matched_items = {}
-    for _, v in ipairs(G.P_CENTER_POOLS["Joker"]) do
-        if string.find(string.lower(v.name), string.lower(G.ARGS['collection_search_input'])) then
-            table.insert(matched_items, v)
-        end
-    end
-    for _, v in ipairs(G.P_CENTER_POOLS["Tarot"]) do
-        if string.find(string.lower(v.name), string.lower(G.ARGS['collection_search_input'])) then
-            table.insert(matched_items, v)
-        end
-    end
-    for _, v in ipairs(G.P_CENTER_POOLS["Spectral"]) do
-        if string.find(string.lower(v.name), string.lower(G.ARGS['collection_search_input'])) then
-            table.insert(matched_items, v)
-        end
-    end
-    for _, v in ipairs(G.P_CENTER_POOLS["Planet"]) do
-        if string.find(string.lower(v.name), string.lower(G.ARGS['collection_search_input'])) then
-            table.insert(matched_items, v)
-        end
-    end
+    local matched_items = get_matched_items()
 
     local options = {}
     for i = 1, math.ceil(#matched_items / (5 * #G.your_collection)) do
@@ -82,28 +85,10 @@ end
 ---@param args {cycle_config: table}
 --**cycle_config** Is the config table from the original option cycle UIE
 G.FUNCS.your_collection_search_page = function(args)
-    local matched_items = {}
-    for _, v in ipairs(G.P_CENTER_POOLS["Joker"]) do
-        if string.find(string.lower(v.name), string.lower(G.ARGS['collection_search_input'])) then
-            table.insert(matched_items, v)
-        end
-    end
-    for _, v in ipairs(G.P_CENTER_POOLS["Tarot"]) do
-        if string.find(string.lower(v.name), string.lower(G.ARGS['collection_search_input'])) then
-            table.insert(matched_items, v)
-        end
-    end
-    for _, v in ipairs(G.P_CENTER_POOLS["Spectral"]) do
-        if string.find(string.lower(v.name), string.lower(G.ARGS['collection_search_input'])) then
-            table.insert(matched_items, v)
-        end
-    end
-    for _, v in ipairs(G.P_CENTER_POOLS["Planet"]) do
-        if string.find(string.lower(v.name), string.lower(G.ARGS['collection_search_input'])) then
-            table.insert(matched_items, v)
-        end
-    end
     if not args or not args.cycle_config then return end
+
+    local matched_items = get_matched_items()
+    
     for j = 1, #G.your_collection do
         for i = #G.your_collection[j].cards, 1, -1 do
             local c = G.your_collection[j]:remove_card(G.your_collection[j].cards[i])
